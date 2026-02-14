f318b21a75cccc19372f496c6a3de397f26ddfd9
DiazPedro696 <pedrodiazz272@gmail.com>
2026-02-14 18:31:11 -0500

    datos trainer

--- Proyecto_Python_DiazPedroSepulvedaKeyner.py
+++ Proyecto_Python_DiazPedroSepulvedaKeyner.py
@@ -6,7 +6,8 @@ import json
 with open("registros_campers_350.json", "r", encoding="utf-8") as archivo: #open = abre el archivo #,"r", para read/leer #"utf-8" = para que no falle con tildes #"as archivo" variable que representa el archivo #with abre el archivo y lo cierra automaticamente
     campers = json.load(archivo) #json.load() sirve para que pase el archivo json a python
 
-
+with open("trainers.json","r", encoding="utf-8") as archivoT:
+     trainers = json.load(archivoT)
 
 def menuRegistro():
     print("--------------------------")
@@ -18,12 +19,81 @@ def menuRegistro():
     opcionRegistro = int(input(": "))
     if opcionRegistro == 1:
         registroCamper()
+    if opcionRegistro == 2: 
+        registroTrainer()
+
+def registroTrainer():
+    nombreTrainer = input("cual es tu nombre? trainer: ")
+    passwordTrainer = input("ingresa tu contraseña: ")
+    for nombreT in trainers:
+        if nombreTrainer.strip().lower() == nombreT["nombre"].strip().lower() and passwordTrainer.strip() == nombreT["password"]: #.strip() para evitar errores por espacios al inicio o al final y .lower() para evitar errores por mayusculas
+            menuTrainer()
+            return # sale de la funcion
+    print("nombre o contraseña incorrectos!")
+        
+     
+def menuTrainer():
+    print("--- BIENVENIDO TRAINER ---")
+    print("1. Asignar notas")
+    print("2. Mirar estudiantes de su grupo")
+    opTrainer = int(input(": "))
+    if opTrainer == 1:
+        asignarNotas()
+
+
+modulos = [
+    "fundamentos de programacion",
+    "programacion web",
+    "programacion formal",
+    "bases de datos",
+    "backend"
+]
+
+
+def asignarNotas():
+    nombreEstudianteasignar = input("ingrese el nombre del estudiante: ").strip()
+        
+    print("\n--- MODULOS ---")
+    for i, modulo in enumerate(modulos, start=1):
+        print("{i}, {modulo}")
+        elegirModulo = int(input("seleccione el modulo: "))
+        moduloSeleccionado = modulos[elegirModulo-1]
+        practica = float(input("nota prueba practica (60%): "))
+        teorica = float(input("nota prueba teorica (30%): "))
+        trabajos = float(input("nota final trabajos (10%): "))
+        notaFinal = (practica*0.6) + (teorica*0.3) + (trabajos*0.1)
+        
+        registro = {
+            "nombre": nombreEstudianteasignar,
+            "modulo": moduloSeleccionado,
+            "notas": {
+                "nota practica": practica,
+                "nota teorica": teorica,
+                "nota trabajos": trabajos,
+                "nota final": round(notaFinal, 2) #redondear numero a 2 decimales
+            } 
+                
+        }
+    
+
+
+def guardarNotas(registro):
+    try: #"intentar" por si el archivo no existe
+        with open("notas.json","r", encoding="utf-8") as archivoN:
+            notas = json.load(archivoN)
+    except FileNotFoundError: #si el archivo no existe, en este caso crea una lista vacia
+        notas = []
+
+    notas.append(registro)
+    with open("notas.json","w", encoding="utf-8") as archivoN:
+        json.dump(notas, archivoN)
+        print("Notas Guardadas!")
+
 
 def registroCamper():
         print("--- Bienvenido Camper ---")
         print("Que quieres hacer? : ")
         print("1. Ver estado de usuario")
-        print("2. Solicitar inscribirse")
         print("3. ver lista de campers")
         opcionCamper = int(input(": "))
 
@@ -36,6 +106,7 @@ def registroCamper():
                      if ni["estado"]["situacion"] == "Cursando":
                           print("estas en riesgo? :",ni["estado"]["en riesgo"])
                      
+
 
         elif opcionCamper == 3:
              for listaCampers in campers:
